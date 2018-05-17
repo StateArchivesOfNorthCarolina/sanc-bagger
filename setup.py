b1b3a91fdd58c2c9728e82d4456fdae2e3db4a36
Gibson <Jeremy.Gibson@ncdcr.gov>
2018-05-17 13:10:58 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SANCTools',
+    version='0.1.0',
+    scripts=['sanc_bagger.py'],
+    install_requires=['bagit'],
+    url='',
+    license='GPLV3',
+    author='Jeremy Gibson',
+    author_email='jeremy.gibson@ncdcr.gov',
+    description=''
+)
